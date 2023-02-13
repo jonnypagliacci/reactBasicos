46bb71bd25d078bef58d10facd3c3ff75708a609
jonnypagliacci <dr.sergio.valdovinos@gmail.com>
2023-02-13 08:50:42 -0600

    Se arregló el problema con los botones de la sección MODULO

--- src/componentes/Modulo.tsx
+++ src/componentes/Modulo.tsx
@@ -1,9 +1,24 @@
-function Modulo() {
+
+interface handle {
+  onConfirm(): unknown;
+  onCancel(): unknown;
+};
+
+function Modulo(props: handle) {
+
+  function handleCancel() {
+    props.onCancel();
+  };
+  function handleConfirm() {
+    props.onConfirm();
+  };
+
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn btn--alt">Cancel</button>
-      <button className="btn">Confirm</button>
+      <button className="btn btn--alt" onClick={handleCancel}>Cancel</button>
+      <button className="btn" onClick={handleConfirm}>Confirm</button>
     </div>
   );
 }
--- src/componentes/Mytodo.tsx
+++ src/componentes/Mytodo.tsx
@@ -3,32 +3,32 @@ import Modulo from "./Modulo";
 import Backdrop from "./Backdrop";
 
 interface Titulo {
-  titulo1: string;
+    titulo1: string;
 }
 
 function Todo(props: Titulo) {
-  const [moduloEstaAbierto, estadoDelModuloAbierto] = useState(false);
+    const [moduloEstaAbierto, estadoDelModuloAbierto] = useState(false);
 
-  function delatarCambio() {
-    return estadoDelModuloAbierto(true);
-  }
+    function delatarCambio() {
+        return estadoDelModuloAbierto(true);
+    }
 
-  function cerrarModulo () {
-    return estadoDelModuloAbierto(false);
-  }
+    function cerrarModulo() {
+        return estadoDelModuloAbierto(false);
+    }
 
-  return (
-    <div className="card">
-      <h2> {props.titulo1} </h2>
-      <div>
-        <button className="btn" onClick={delatarCambio}>
-          Delete
-        </button>
-      </div>
-      {moduloEstaAbierto ? <Modulo /> : null}
-      {moduloEstaAbierto ? <Backdrop alClick1={cerrarModulo}/> : null}
-    </div>
-  );
+    return (
+        <div className="card">
+            <h2> {props.titulo1} </h2>
+            <div>
+                <button className="btn" onClick={delatarCambio}>
+                    Delete
+                </button>
+            </div>
+            {moduloEstaAbierto ? <Modulo onCancel={cerrarModulo} onConfirm={cerrarModulo} /> : null}
+            {moduloEstaAbierto ? <Backdrop alClick1={cerrarModulo} /> : null}
+        </div>
+    );
 }
 
 export default Todo;
